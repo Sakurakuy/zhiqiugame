db9c109cd25b369fa0fd77312ca34d19dcc8e7fa
Sakurakuy <127234246+Sakurakuy@users.noreply.github.com>
2025-02-04 03:13:57 +0800

    Add files via upload

---
+++ games.js
@@ -0,0 +1,11 @@
+const gamesList = [
+    {
+        title: "游戏1",
+        link: "https://pan.baidu.com/s/your_link_1"
+    },
+    {
+        title: "游戏2",
+        link: "https://pan.baidu.com/s/your_link_2"
+    }
+    // 您可以随时在这里添加更多游戏
+]; 
