735cb016a5b6d04e8b812df10f472c494bd940a4
Sakurakuy <127234246+Sakurakuy@users.noreply.github.com>
2025-02-04 04:00:51 +0800

    Update games.js

--- games.js
+++ games.js
@@ -2,12 +2,17 @@ const gamesList = [
     {
         title: "游戏1",
         pcLink: "https://www.example.com/pc-game1", // 替换为实际的电脑下载链接
-        androidLink: "https://www.example.com/android-game1" // 替换为实际的安卓下载链接
+        androidLink: null // 表示没有安卓下载链接
     },
     {
         title: "游戏2",
-        pcLink: "https://www.example.com/pc-game2", // 替换为实际的电脑下载链接
+        pcLink: null, // 表示没有电脑下载链接
         androidLink: "https://www.example.com/android-game2" // 替换为实际的安卓下载链接
+    },
+    {
+        title: "游戏3",
+        pcLink: "https://www.example.com/pc-game3",
+        androidLink: "https://www.example.com/android-game3"
     }
     // 可以继续添加更多游戏
 ];
