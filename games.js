3e21dec57ffaf7fbac21330b2b105ae87ccc9c34
Sakurakuy <127234246+Sakurakuy@users.noreply.github.com>
2025-02-04 18:58:09 +0800

    Update games.js

--- games.js
+++ games.js
@@ -1,8 +1,8 @@
 const gamesList = [
     {
-        title: "游戏1",
-        pcLink: "https://www.example.com/pc-game1", // 替换为实际的电脑下载链接
-        androidLink: "https://www.example.com/android-game1" // 替换为实际的安卓下载链接
+        title: "腐败王国0.22.1",
+        pcLink: "https://pan.baidu.com/s/1cBXy-jj6OjtGFyRaZTv6SA?pwd=9s6l", // 替换为实际的电脑下载链接
+        androidLink: "https://pan.baidu.com/s/1wvwty_z7KGLj_pg5WqE9uA?pwd=rmja" // 替换为实际的安卓下载链接
     },
     {
         title: "游戏2",
