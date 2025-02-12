5220540d177aa4b869f8f0d16af7eca6c1fc3275
Sakurakuy <127234246+Sakurakuy@users.noreply.github.com>
2025-02-12 23:03:40 +0800

    Update games.js

--- games.js
+++ games.js
@@ -1,18 +1,18 @@
 const gamesList = [
     {
-        title: "腐败王国0.22.1",
+        title: "腐败王国v0.22.1",
         pcLink: "https://pan.baidu.com/s/1cBXy-jj6OjtGFyRaZTv6SA?pwd=9s6l", // 替换为实际的电脑下载链接
         androidLink: "https://pan.baidu.com/s/1wvwty_z7KGLj_pg5WqE9uA?pwd=rmja" // 替换为实际的安卓下载链接
     },
     {
-        title: "游戏2",
-        pcLink: null, // 表示没有电脑下载链接
-        androidLink: "https://www.example.com/android-game2" // 替换为实际的安卓下载链接
+        title: "永恒世界v0.8.5",
+        pcLink: "https://pan.baidu.com/s/1IqKnya5K3C112WD8dH-HmA?pwd=djw2", // 表示没有电脑下载链接
+        androidLink: "https://pan.baidu.com/s/1Lj46SIk-nZjqayMy_eOoKg?pwd=e8er" // 替换为实际的安卓下载链接
     },
     {
-        title: "游戏3",
-        pcLink: "https://www.example.com/pc-game3",
-        androidLink: "https://www.example.com/android-game3"
+        title: "特工17v0.24.8",
+        pcLink: "https://pan.baidu.com/s/1PgFxkaMARSXK2EJT3QJOCg?pwd=8it2",
+        androidLink: "https://pan.baidu.com/s/1Z903-HwxmeCY2GC2TP5X7w?pwd=bqdr"
     }
     // 可以继续添加更多游戏
 ];
